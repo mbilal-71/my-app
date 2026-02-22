914c3f54a4853c1463efa77ca48309dbb06374e4
Muhammad Bilal <mb16837157@gmail.com>
2026-02-22 06:51:56 +0500

    react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      {/* <Navbar title="React App" aboutText="About Us" /> */}
       <Navbar />
     </>
   );
